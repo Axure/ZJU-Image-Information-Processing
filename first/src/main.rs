acca794bbe7b6e18075b4e44db65674075bb131e
Hu Zheng <freetiger18@gmail.com>
2017-03-30 20:36:46 +0800

    Duplicate the bmp file.

--- first/src/main.rs
+++ first/src/main.rs
@@ -1,8 +1,40 @@
+extern crate memmap;
 
 mod bmp;
+
 use bmp::BmpFile;
 
 fn main() {
-    let a = BmpFile::new();
-    println!("Hello, world!");
+    use std::io::Write;
+    use memmap::{Mmap, Protection};
+
+    let original_mmap = Mmap::open_path("resources/colorful.bmp", Protection::Read).unwrap();
+    let original_bytes: &[u8] = unsafe { original_mmap.as_slice() };
+    println!("{:?}", &original_bytes[0..8]);
+    println!("{:?}", &original_bytes[10..14]);
+    println!("{:?}", &original_bytes[138..142]);
+    println!("{:?}", &original_bytes[142..146]);
+    println!("{:?}", original_bytes.len());
+
+    use std::fs::File;
+    use std::io::prelude::*;
+
+    let mut file = File::create("resources/foo.bmp").unwrap();
+    unsafe {file.set_len(original_bytes.len() as u64);}
+    file.sync_all();
+
+    let mut duplicated_mmap = Mmap::open_path("resources/foo.bmp", Protection::ReadWrite).unwrap();
+    {
+        let duplicated_bytes: &[u8] = unsafe { duplicated_mmap.as_slice() };
+        println!("{:?}", duplicated_bytes.len());
+        println!("{:?}", &duplicated_bytes[0..8]);
+    }
+    unsafe { duplicated_mmap.as_mut_slice() }.write(original_bytes).unwrap();
+    {
+        let anon_bytes: &[u8] = unsafe { duplicated_mmap.as_slice() };
+        println!("{:?}", anon_bytes.len());
+        println!("{:?}", &anon_bytes[0..8]);
+    }
+    duplicated_mmap.flush();
+
 }
